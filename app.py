d12c87b2e31d2aa7228e9ad1971f6aabf3b19bf9
sanjay07s <136256345+sanjay07s@users.noreply.github.com>
2024-09-23 05:01:03 +0000

    added dynamic data and approut

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Chennai, India',
+    'salary': 'Rs. 20,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Kerala, India',
+    'salary': 'Rs. 30,00,000'
+  },
+  {
+    'id': 4,
+    'title': 'ML Engineer',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 40,00,000'
+  }
+]
 @app.route("/")
-def hello_world():
-  return 'hello world'
+def hello_world( ):
+  return render_template('home.html',jobs = JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug = "True")
+  app.run(host='0.0.0.0', debug = 'True')
